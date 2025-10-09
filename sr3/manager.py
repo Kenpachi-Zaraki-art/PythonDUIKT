fccb152500756c9e6663ca72a85abe1783a9fece
Kenpachi-Zaraki-art <top4ikrudua@gmail.com>
2025-10-09 09:17:18 +0300

    Додав геттер і сеттер для кількості підлеглих

--- sr3/manager.py
+++ sr3/manager.py
@@ -6,3 +6,8 @@ class Manager(Employee):
     def __init__(self, name, monthly_salary, worked_days, subordinates, bonus_percent=0):
         super().__init__(name, monthly_salary, worked_days, bonus_percent)
         self._subordinates = subordinates
+    def get_subordinates(self):
+        return self._subordinates
+
+    def set_subordinates(self, count):
+        self._subordinates = count
