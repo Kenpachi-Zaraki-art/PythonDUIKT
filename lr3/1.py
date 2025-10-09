56fcb8af03174da7f12bf1bfd14900599075b5d9
Kenpachi-Zaraki-art <top4ikrudua@gmail.com>
2025-10-09 08:51:10 +0300

    Основна програма

--- lr3/1.py
+++ lr3/1.py
@@ -8,3 +8,13 @@ class Person:
         return f"Студент: {self.first_name} {self.last_name}, оцінка: {self.grade}"
     def __del__(self):
         print(f"Ви отримали стипендію {self.first_name} {self.last_name}!")
+student1 = Person("Іван", "Петренко", 5)
+student2 = Person("Марія", "Коваленко", 4)
+student3 = Person("Денис", "Руденок")  # оцінка за замовчуванням = 1
+
+print(student1.get_info())
+print(student2.get_info())
+print(student3.get_info())
+
+print("\nОголошення про стипендію:")
+print("Всі студенти отримали стипендію!")
