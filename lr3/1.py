46b1090a63ac6e7ca6e5a2ebea887a0205a400d1
Kenpachi-Zaraki-art <top4ikrudua@gmail.com>
2025-10-09 08:49:55 +0300

    додано метод get_info

--- lr3/1.py
+++ lr3/1.py
@@ -1,6 +1,8 @@
 class Person:
     def __init__(self, first_name, last_name, grade=1):
-        """Ініціалізація атрибутів студента"""
         self.first_name = first_name
         self.last_name = last_name
         self.grade = grade
+
+    def get_info(self):
+        return f"Студент: {self.first_name} {self.last_name}, оцінка: {self.grade}"
