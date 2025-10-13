c9b4b5ec98712fd04d7e63314189f4ee8eb6e805
Kenpachi-Zaraki-art <top4ikrudua@gmail.com>
2025-10-13 20:27:28 +0300

    Створення об'єкта класу Manager, додавання об'єктів до списку співробітників

--- sr3/main.py
+++ sr3/main.py
@@ -4,3 +4,7 @@ from manager import Manager
 
 emp1 = Employee("Alice", 15000, 28, 5)
 emp2 = Employee("Bob", 12000, 25, 3)
+
+mgr1 = Manager("Charlie", 20000, 30, subordinates=5, bonus_percent=10)
+
+employees = [emp1, emp2, mgr1]
