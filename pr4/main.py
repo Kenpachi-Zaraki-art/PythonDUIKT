084674e025a1aad3f2b76ea3127b612857589ff9
Kenpachi-Zaraki-art <top4ikrudua@gmail.com>
2025-10-14 08:19:41 +0300

    Ініціалізація та створення головного вікна.

---
+++ pr4/main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+import random
+import time
+from Catcher import Catcher
+from Score import Score
+from Egg import Egg
+
+tk = Tk()
+tk.title("Гра: Ловець!")
+tk.resizable(0, 0)
+tk.wm_attributes("-topmost", 1)
+
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
+canvas.pack()
+
