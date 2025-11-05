3332ebcd6f084bdf490a948ce987ffe0ba7cb209
Kenpachi-Zaraki-art <top4ikrudua@gmail.com>
2025-11-05 21:57:01 +0200

    Реалізовано гетери та сетери для ПІБ та номера групи

--- sr4/student.py
+++ sr4/student.py
@@ -4,3 +4,26 @@ class Student:
         self.__group_number = group_number
         self.__dob = dob  # Дата народження
         self.__address = address  # Адреса
+
+        # --- Гетер для ПІБ ---
+        @property
+        def pib(self) -> str:
+            return self.__pib
+
+        # --- Сетер для ПІБ ---
+        @pib.setter
+        def pib(self, new_pib: str):
+            if isinstance(new_pib, str) and len(new_pib) > 5:  # Проста валідація
+                self.__pib = new_pib
+            else:
+                print("Помилка: ПІБ має бути рядком довжиною більше 5 символів.")
+
+        # --- Гетер для Номера групи ---
+        @property
+        def group_number(self) -> str:
+            return self.__group_number
+
+        # --- Сетер для Номера групи ---
+        @group_number.setter
+        def group_number(self, new_group: str):
+            self.__group_number = new_group
