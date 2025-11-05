b796a1d2ebeb5d09b0c633f63160bd4110ca1a87
Kenpachi-Zaraki-art <top4ikrudua@gmail.com>
2025-11-05 22:00:47 +0200

    Реалізовано клас DesiredPerformance з власною логікою

--- sr4/perfomance.py
+++ sr4/perfomance.py
@@ -28,3 +28,36 @@ class RealPerformance(Performance):
         if not self.grades:
             return 0.0
         return sum(self.grades) / len(self.grades)
+
+
+class DesiredPerformance(Performance):
+    """
+    Клас БАЖАНА_УСПІШНІСТЬ, наслідує Performance.
+    Список балів тут - це "Список бажаних балів".
+    """
+
+    def __init__(self, subjects: List[str], grades: List[int], desired_avg_input: float = None):
+        super().__init__(subjects, grades)
+        # Додаткове поле для "введеного студентом" середнього балу
+        self.__desired_average_input = desired_avg_input
+
+    def average_grade(self) -> float:
+        """
+        Повертає бажаний середній бал, введений студентом.
+        Якщо він не введений, для демонстрації обчислимо середній з бажаних балів.
+        """
+        if self.__desired_average_input is not None:
+            return self.__desired_average_input  #
+
+        # Альтернативна логіка, якщо бал не введено
+        if not self.grades:
+            return 0.0
+        return sum(self.grades) / len(self.grades)
+
+    @property
+    def desired_average(self) -> float:
+        return self.average_grade()
+
+    @desired_average.setter
+    def desired_average(self, value: float):
+        self.__desired_average_input = value
