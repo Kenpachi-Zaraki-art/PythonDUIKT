1919b1e68fcfc700e84e3b65d33def48e45b0fdf
Kenpachi-Zaraki-art <top4ikrudua@gmail.com>
2025-11-05 22:00:22 +0200

    Реалізовано клас RealPerformance

--- sr4/perfomance.py
+++ sr4/perfomance.py
@@ -16,3 +16,15 @@ class Performance(ABC):
         Абстрактний метод для обчислення середнього балу.
         """
         pass
+class RealPerformance(Performance):
+    """
+    Клас для реальної успішності.
+    Реалізує метод average_grade для обчислення реального середнього балу.
+    """
+    def average_grade(self) -> float:
+        """
+        Обчислює реальний середній бал.
+        """
+        if not self.grades:
+            return 0.0
+        return sum(self.grades) / len(self.grades)
